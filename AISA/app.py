3e2b5158413f2c47f16079a861577a8d52015f3b
MILORDB <leosdanicuba@gmail.com>
2026-07-24 22:25:54 +0200

    Delete AISA directory

--- AISA/app.py
+++
@@ -1,6 +0,0 @@
-# app.py - Punto de entrada para Render
-from web_admin.app import app
-
-# Esto es todo lo que necesita Render
-if __name__ == "__main__":
-    app.run()
--- AISA/modules/__init__.py
+++
@@ -1,4 +0,0 @@
-# modules/__init__.py
-from modules.android_conexion import AndroidConexion, get_android_conexion
-
-__all__ = ['AndroidConexion', 'get_android_conexion']
