b84aafe45dce2f3a1bb82dfb1aae9bcdfca710f9
MILORDB <leosdanicuba@gmail.com>
2026-07-27 01:49:11 +0200

    Create __init__.py

---
+++ web_admin/__init__.py
@@ -0,0 +1,13 @@
+# web_admin/__init__.py
+from web_admin.storage import StorageManager, get_storage_manager
+from web_admin.database import get_db
+from web_admin.auth import crear_sesion, verificar_sesion, obtener_usuario_sesion
+
+__all__ = [
+    'StorageManager', 
+    'get_storage_manager',
+    'get_db',
+    'crear_sesion',
+    'verificar_sesion',
+    'obtener_usuario_sesion'
+]
