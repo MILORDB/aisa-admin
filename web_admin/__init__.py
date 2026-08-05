1d7300b54d57a235ce22df717a8d969f6b4b1075
MILORDB <leosdanicuba@gmail.com>
2026-08-04 22:53:33 -0400

    Update __init__.py

--- web_admin/__init__.py
+++ web_admin/__init__.py
@@ -2,8 +2,10 @@
 from web_admin.storage import StorageManager, get_storage_manager
 from web_admin.database import get_db
 from web_admin.auth import crear_sesion, verificar_sesion, obtener_usuario_sesion
+from web_admin.app import app
 
 __all__ = [
+    'app',
     'StorageManager', 
     'get_storage_manager',
     'get_db',
